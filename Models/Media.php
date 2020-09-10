a7bbc2b672d1339dddfdadcb2b11422595011de6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-10 20:40:12 +0200

    fix typehint

--- Models/Media.php
+++ Models/Media.php
@@ -311,11 +311,11 @@ class Media implements \JsonSerializable
     }
 
     /**
-     * @return \DateTime
+     * @return \DateTimeImmutable
      *
      * @since 1.0.0
      */
-    public function getCreatedAt() : \DateTimeInterface
+    public function getCreatedAt() : \DateTimeImmutable
     {
         return $this->createdAt;
     }
