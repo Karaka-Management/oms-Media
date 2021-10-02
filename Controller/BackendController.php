e6165b83ae528b9c5c2c379f97741fd269fb9ffc
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-03 01:25:38 +0200

    bug fixes

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -35,49 +35,10 @@ use phpOMS\Views\View;
  * @license OMS License 1.0
  * @link    https://orange-management.org
  * @since   1.0.0
+ * @codeCoverageIgnore
  */
 final class BackendController extends Controller
 {
-    /**
-     * Module path.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const PATH = __DIR__;
-
-    /**
-     * Module version.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const VERSION = '1.0.0';
-
-    /**
-     * Module name.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const NAME = 'Media';
-
-    /**
-     * Module id.
-     *
-     * @var int
-     * @since 1.0.0
-     */
-    public const ID = 1000400000;
-
-    /**
-     * Providing.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static array $providing = [];
-
     /**
      * Dependencies.
      *
