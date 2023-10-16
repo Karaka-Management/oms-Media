7883166e7ed1dd4acceaef927bb317e85c87d318
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:25:58 +0000

    auto generate NullTests

---
+++ tests/Models/NullReferenceTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Media\tests\Models;
+
+use Modules\Media\Models\NullReference;
+
+/**
+ * @internal
+ */
+final class NullReferenceTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Media\Models\NullReference
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Media\Models\Reference', new NullReference());
+    }
+
+    /**
+     * @covers Modules\Media\Models\NullReference
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullReference(2);
+        self::assertEquals(2, $null->getId());
+    }
+
+    /**
+     * @covers Modules\Media\Models\NullReference
+     * @group framework
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullReference(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
